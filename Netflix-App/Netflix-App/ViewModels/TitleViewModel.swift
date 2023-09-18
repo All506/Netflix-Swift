58ef459d7aee41b0c49b8f684cd2685177284c82
Allan Trejos <allcincoceroseis@gmail.com>
2023-09-18 11:41:53 -0600

    fet(App):Coming Soon Screen
    Coming Soon screen has been created and implemented correctly

---
+++ Netflix-App/Netflix-App/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix-App
+//
+//  Created by Allan Trejos Salazar on 18/9/23.
+//
+
+import Foundation
+
+struct TitleViewModel{
+    let titleName: String
+    let posterURL: String
+}
